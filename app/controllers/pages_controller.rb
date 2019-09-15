9de9b9b2557ac1cddff8e16990da543c91b42092
Mark Thomas <mdvthomas@gmail.com>
2019-09-15 19:25:34 -0400

    Integrated skills via the model generator

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
     @posts = Blog.all
+    @skills = Skill.all # places value of SKills db query into the instance var, ready to be used in the view.
   end
 
   def about
