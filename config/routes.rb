717fdd71e812bd62cf979c2b2b5258127ad0ce00
Mark Thomas <mdvthomas@gmail.com>
2019-09-22 21:26:43 -0400

    Customized Portfolio routes URI pattern and route method.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  # except: provide all routes except...
+  resources :portfolios, except: [:show]
+
+  # override URI pattern & add custom route method (now it appears as portfolio/1 rather than the plural)
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show' # as: = add custom route method
 
   # get method '/url', to: Controller#Action
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'test', to: 'pages#test'
 
   resources :blogs
 
