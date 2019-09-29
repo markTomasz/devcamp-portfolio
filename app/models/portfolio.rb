1af63e32c52f82a353ccb4de4d5b2bc5d4a220e6
Mark Thomas <mdvthomas@gmail.com>
2019-09-29 18:13:07 -0400

    implemented image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,16 @@ class Portfolio < ApplicationRecord
   end
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: "Ruby on Rails") }
+
+  # automatically runs when NEW action happens in portfolios.
+  # sets default values for new portfolio items.
+  after_initialize :set_defaults
+
+  def set_defaults
+    # '=' will override anything put in manually. 
+    # '||=' is 'if self.main_image == nil {self.main_image = 'https://...}
+    # basically a conditional =
+    self.main_image ||= 'https://via.placeholder.com/600x400'
+    self.thumb_image ||= 'https://via.placeholder.com/350x200'
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,57 +12,56 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_27_192806) do
-
+ActiveRecord::Schema.define(version: 20_190_927_192_806) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "blogs", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "slug"
-    t.integer "status", default: 0
-    t.bigint "topic_id"
-    t.index ["slug"], name: "index_blogs_on_slug", unique: true
-    t.index ["topic_id"], name: "index_blogs_on_topic_id"
+  create_table 'blogs', force: :cascade do |t|
+    t.string 'title'
+    t.text 'body'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.string 'slug'
+    t.integer 'status', default: 0
+    t.bigint 'topic_id'
+    t.index ['slug'], name: 'index_blogs_on_slug', unique: true
+    t.index ['topic_id'], name: 'index_blogs_on_topic_id'
   end
 
-  create_table "friendly_id_slugs", force: :cascade do |t|
-    t.string "slug", null: false
-    t.integer "sluggable_id", null: false
-    t.string "sluggable_type", limit: 50
-    t.string "scope"
-    t.datetime "created_at"
-    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
-    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
-    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
-    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
+  create_table 'friendly_id_slugs', force: :cascade do |t|
+    t.string 'slug', null: false
+    t.integer 'sluggable_id', null: false
+    t.string 'sluggable_type', limit: 50
+    t.string 'scope'
+    t.datetime 'created_at'
+    t.index %w[slug sluggable_type scope], name: 'index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope', unique: true
+    t.index %w[slug sluggable_type], name: 'index_friendly_id_slugs_on_slug_and_sluggable_type'
+    t.index ['sluggable_id'], name: 'index_friendly_id_slugs_on_sluggable_id'
+    t.index ['sluggable_type'], name: 'index_friendly_id_slugs_on_sluggable_type'
   end
 
-  create_table "portfolios", force: :cascade do |t|
-    t.string "title"
-    t.string "subtitle"
-    t.text "body"
-    t.text "main_image"
-    t.text "thumb_image"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'portfolios', force: :cascade do |t|
+    t.string 'title'
+    t.string 'subtitle'
+    t.text 'body'
+    t.text 'main_image'
+    t.text 'thumb_image'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "skills", force: :cascade do |t|
-    t.string "title"
-    t.integer "percent_utilized"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'skills', force: :cascade do |t|
+    t.string 'title'
+    t.integer 'percent_utilized'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "topics", force: :cascade do |t|
-    t.string "title"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'topics', force: :cascade do |t|
+    t.string 'title'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  add_foreign_key "blogs", "topics"
+  add_foreign_key 'blogs', 'topics'
 end
