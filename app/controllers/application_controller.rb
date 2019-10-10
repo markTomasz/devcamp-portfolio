d1512c72b94c64686ceec759839c20257dec2ec7
Mark Thomas <mdvthomas@gmail.com>
2019-10-09 22:36:03 -0400

    Added session tracker for dynamic source links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
- include DeviseWhitelist
+  include DeviseWhitelist
+  before_action :set_source
+
+  def set_source
+    # by passing "?q=twitter" in the URL, params picks up a new key/value pair.
+    # below, we create a new session hash called "source" and set it to the param "q"
+    session[:source] = params[:q] if params[:q]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'angular-items', to: 'portfolios#angular'
 
   # override URI pattern & add custom route method (now it appears as portfolio/1 rather than the plural)
+  # ***** route below: takes in an ID parameter, then mapped to the show action
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show' # as: = add custom route method
 
   # get method '/url', to: Controller#Action
