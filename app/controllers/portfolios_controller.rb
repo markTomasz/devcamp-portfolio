92117e61f6e67ad005c168d42748624bee0f09f3
Mark Thomas <mdvthomas@gmail.com>
2019-11-17 15:31:46 -0500

    user model changed for exercise

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:show, :edit, :update, :destroy]
   layout 'portfolio'
 
   # when you want to list a number of items
@@ -11,7 +12,6 @@ class PortfoliosController < ApplicationController
   end
 
   def show
-    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def new
@@ -35,12 +35,9 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def update
-    @portfolio_item = Portfolio.find(params[:id])
-
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'Record was successfully updated.' }
@@ -72,5 +69,9 @@ class PortfoliosController < ApplicationController
                                       :subtitle,
                                       :body,
                                       technologies_attributes: [:name])
+  end
+
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,25 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates_presence_of :name
+  # validates_presence_of :name
 
   def first_name
-    name.split.first
+    begin
+      name.split.first.oops
+    rescue
+    end
   end
 
   def last_name
     name.split.last
+  end
+end
+
+def get_facebook_messages
+  begin
+    contacts_fb.oops
+    @messaged = retrieves_messages
+  rescue IOError => e
+    flash[:error] = "Error occured contacting Facebook: #{e}"
   end
 end
