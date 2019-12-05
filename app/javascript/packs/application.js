12933594a6dbf82ed5c9329ff7df39e73c788008
Mark Thomas <mdvthomas@gmail.com>
2019-12-04 22:31:41 -0500

    installed jquery ui, sortable html5 lib, and css calls

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,12 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+
 require("jquery")
 require("bootstrap")
 
+//require("jquery-ui")
+require("packs/html.sortable")
 //= require jquery
 //= require bootstrap-sprockets
 
