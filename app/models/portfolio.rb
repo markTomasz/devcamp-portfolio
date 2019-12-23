9d5ccfe41ccdc1ec294c5d9e3a412bd68a9b5067
Mark Thomas <mdvthomas@gmail.com>
2019-12-23 15:21:22 -0500

    updated file upload forms and refactored placeholder functionality into view

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -7,8 +7,7 @@ class Portfolio < ApplicationRecord
   # console: Portfolio.create!(title: 'fsdjfds', body: 'dkshfkdsjf', technologies_attributes: [{}, {}, {}])
   accepts_nested_attributes_for :technologies, reject_if: lambda { |attrs| attrs['name'].blank? }
 
-  include Placeholder
-  validates_presence_of :title, :body, :main_image, :thumb_image
+  validates_presence_of :title, :body
 
   mount_uploader :thumb_image, PortfolioUploader
   mount_uploader :main_image, PortfolioUploader
@@ -22,16 +21,6 @@ class Portfolio < ApplicationRecord
   end
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: "Ruby on Rails") }
-
-  # automatically runs when NEW action happens in portfolios.
-  # sets default values for new portfolio items.
-  after_initialize :set_defaults
-
-  def set_defaults
-    # '=' will override anything put in manually. 
-    # '||=' is 'if self.main_image == nil {self.main_image = 'https://...}
-    # basically a conditional =
-    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-    self.thumb_image ||= Placeholder.image_generator(height: '300', width: '200')
-  end
 end
+
+
